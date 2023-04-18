aad1e9f0b45b21204d6bc3d642c36e87603b2a0a
M. Abdullah Butt <81871162+AbdullahButt2611@users.noreply.github.com>
2023-04-18 18:28:39 +0500

    Responsive Navigation Bar

---
+++ Responsive Navigation Bar/menu.js
@@ -0,0 +1,10 @@
+var menu = document.querySelector('.Menu');
+
+function ToggleMenu(){
+    if (menu.classList.contains('full')){
+    menu.classList.remove('full');
+}else{
+    menu.classList.add('full');
+}
+}
+
