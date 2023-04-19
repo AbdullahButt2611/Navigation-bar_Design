43e2f8f995bc616ec770ed894ab795eefcafde11
M. Abdullah Butt <81871162+AbdullahButt2611@users.noreply.github.com>
2023-04-20 00:11:55 +0500

    Designing the Content Section

--- Horizontal Scrollable & Draggable Tab Navigation Menu/main.js
+++ Horizontal Scrollable & Draggable Tab Navigation Menu/main.js
@@ -6,22 +6,46 @@ const btnRight = document.querySelector(".right-btn");
 const tabMenu = document.querySelector(".tab-menu");
 
 const IconVisibility = () => {
-    let scrollLeftValue = tabMenu.scrollLeft;
+    let scrollLeftValue = Math.ceil(tabMenu.scrollLeft);
+
+    let scrollableWidth = tabMenu.scrollWidth - tabMenu.clientWidth;
 
     btnLeft.style.display = scrollLeftValue > 0 ? "block" : "none";
+    btnRight.style.display = scrollableWidth > scrollLeftValue ? "block" : "none";
+
 }
 
 
 btnRight.addEventListener("click", () => {
     tabMenu.scrollLeft += 150;
-    IconVisibility();
+    setTimeout(() => {
+        IconVisibility();
+    }, 50);
 });
 
 
 btnLeft.addEventListener("click", () => {
     tabMenu.scrollLeft -= 150;
-    IconVisibility();
+    setTimeout(() => {
+        IconVisibility();
+    }, 50);
 });
+
+
+window.onload = function(){
+    btnRight.style.display = tabMenu.scrollWidth > tabMenu.clientWidth || tabMenu.scrollWidth >= window.innerWidth ? "block" : "none";
+    btnLeft.style.display = tabMenu.scrollWidth >= window.innerWidth ? "" : "none";
+}
+
+
+window.onresize = function(){
+    btnRight.style.display = tabMenu.scrollWidth > tabMenu.clientWidth || tabMenu.scrollWidth >= window.innerWidth ? "block" : "none";
+    btnLeft.style.display = tabMenu.scrollWidth >= window.innerWidth ? "" : "none";
+
+    let scrollLeftValue = Math.round(tabMenu.scrollLeft);
+
+    btnLeft.style.display = scrollLeftValue > 0 ? "block" : "none";
+}
 
 
 
@@ -30,6 +54,34 @@ btnLeft.addEventListener("click", () => {
 
 
 //Javascript to make the tab navigation draggable
+
+let activeDrag = false;
+tabMenu.addEventListener("mousemove", (drag)=>{
+    
+    if(!activeDrag) return;
+
+    tabMenu.scrollLeft -= drag.movementX;
+    IconVisibility();
+    tabMenu.classList.add("dragging");
+    
+})
+
+tabMenu.addEventListener("mouseup", ()=>{
+    
+    activeDrag = false;
+    tabMenu.classList.remove("dragging");
+
+})
+
+tabMenu.addEventListener("mousedown", ()=>{
+    
+    activeDrag = true;
+
+})
+
+
+
+
 
 //Javascript to view tab contents on click tab buttons
 
